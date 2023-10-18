7955e7273d724fba2efca18b6b74002892fe9dce
Zhana Orkodashvili <72037472+zhana-orkodashvili@users.noreply.github.com>
2023-10-19 00:01:22 +0400

    Add files via upload

---
+++ code/main.py
@@ -0,0 +1,26 @@
+import nlp_final_textrank as f
+import nlp_final_lexrank as f1
+from stops import stops_new as stops
+import pandas as pd
+fl = '/home/manjaro/Desktop/python/nlp_final/data/imedi_news.xlsx'
+fl_write = '/home/manjaro/Desktop/python/nlp_final/data/imedi_news_summ.xlsx'
+fl_write1 = '/home/manjaro/Desktop/python/nlp_final/data/imedi_news_summ_diff.xlsx'
+fl_write2 = '/home/manjaro/Desktop/python/nlp_final/data/imedi_news_summ_same.xlsx'
+fl_write3 = '/home/manjaro/Desktop/python/nlp_final/data/imedi_news_summ_diff_lex.xlsx'
+
+
+df = pd.read_excel(fl)
+st = df['სტატია']
+df['textRankStopWords'] = [f.textrank_summary(i, stops =  stops) for i in df['სტატია']]
+df['textRank'] = [f.textrank_summary(i) for i in df['სტატია']]
+df['lexRankStopWord'] = [f1.lexrank_summary(i, stops =  stops) for i in df['სტატია']]
+df['lexRank'] = [f1.lexrank_summary(i) for i in df['სტატია']]
+
+df.to_excel(fl_write)
+
+df[df['textRankStopWords'] != df['textRank']].to_excel(fl_write1)
+
+df[df['textRank'] == df['lexRank']].to_excel(fl_write2)
+
+df[df['lexRankStopWord'] != df['lexRank']].to_excel(fl_write3)
+
